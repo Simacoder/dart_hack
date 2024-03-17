88825ce3b5a4f0bd4066f905198768bfb9d74bb3
Simanga <simacoder@hotmail.com>
2024-03-17 22:46:39 +0200

    dart hackathon assignment committed

---
+++ function.dart
@@ -0,0 +1,20 @@
+// create a progam of mathematical operation 
+
+// addition function 
+int add(int a, int b) {
+  return a + b;
+}
+
+// multiplication function
+int multiply(int a, int b) {
+  return a * b;
+}
+
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+  
+  print("Sum: ${add(num1, num2)}");
+  print("Product: ${multiply(num1, num2)}");
+}
+
---
+++ program1.dart
@@ -0,0 +1,10 @@
+// Display personal information 
+
+void main() {
+  String? name = "Simanga";
+  int? age = 55;
+  String? school = "PLP School";
+  String? hobby = "Playing Chess";
+  
+  print("My name is $name. I am $age years old, and I attend $school. My hobby is $hobby.");
+}
